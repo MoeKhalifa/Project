f402946458d4af323c57af2b7da08e1b16390ddb
Moe.Khalifa <mkhalifa95@hotmail.com>
2020-08-20 01:31:31 +1000

    added random number

--- localusers/luser-demo02.sh
+++ localusers/luser-demo02.sh
@@ -2,3 +2,13 @@
 
 echo "My UID is ${UID}"
 
+USER_NAME=$(id -un)
+
+echo "My username is ${USER_NAME}"
+
+echo "This is a random number: ${RANDOM}"
+
+echo "This is also a random number: ${RANDOM}"
+
+
+
